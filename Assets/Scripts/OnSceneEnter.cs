2202c18df4449d647a16ff768f113537f0c832a5
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-11-22 20:36:50 +0100

    First version

---
+++ Assets/Scripts/OnSceneEnter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class OnSceneEnter : MonoBehaviour {
+
+    public Image BlackImage;
+    public float FadeTime;
+    public bool ActivateFade = true;
+    private bool isFading = true;
+    private float alpha = 1;
+    private float alphaSpeed;
+
+    // Update is called once per frame
+    void Start ()
+    {
+        if(ActivateFade)
+        {
+            alphaSpeed = 1 / FadeTime;
+            StartCoroutine(Fade());
+        }
+    }
+
+    IEnumerator Fade()
+    {
+        //yield return new WaitForSeconds(0.40f);
+        while (isFading)
+        {
+            alpha -= alphaSpeed * Time.deltaTime;
+            if (alpha <= 0)
+            {
+                alpha = 0;
+                isFading = false;
+            }
+            Color myAlphaColor = BlackImage.color;
+            myAlphaColor.a = alpha;
+
+            BlackImage.color = myAlphaColor;
+            yield return new WaitForFixedUpdate();
+        } 
+        alpha = 1;
+        isFading = true;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : CombatStats
+{
+
+    [SerializeField] Navegation nav;
+
+    private void Start()
+    {
+        nav = GetComponent<Navegation>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~13)) //layer 13 click detection
+            {
+                nav.SetDestinationPlayerAndCost(hit.point); //update movements and move
+            }
+        }
+	}
+}
