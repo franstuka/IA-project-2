c16f3e89882f931dbf66e61326672bdbfd862067
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-11-25 14:40:57 +0100

    añadir cosas

---
+++ Assets/Scripts/ScenarioGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioGenerator : MonoBehaviour {
+
+    ////GOOOOO FOR IT
+
+    [SerializeField] Color MontainColor;
+    [SerializeField] Color WoodsColor;
+    [SerializeField] Color PlainColor;
+    [SerializeField] Color MinesColor;
+    [SerializeField] Color CastleColor;
+    [SerializeField] Color HillsColor;
+    [SerializeField] float textureResolution;
+    private Texture2D mapTexture;
+
+    private void CreateTexture()
+    {
+
+    }
+    
+    private void CreateScenatio()
+    {
+        float cellRadius = GridMap.instance.GetCellRadius();
+        int numCellsX = GridMap.instance.GetGridSizeX();
+        int numCellsY = GridMap.instance.GetGridSizeY();
+
+        
+    }
+}
