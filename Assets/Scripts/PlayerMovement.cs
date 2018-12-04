43d00b1da45a0c731d4df6e37674001e4940ef76
Jaume Sanz Sempere <al315564@uji.es>
2018-12-04 11:20:48 +0100

    intento de movimiento

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : CombatStats
 {
 
     [SerializeField] Navegation nav;
+    [SerializeField] bool selected = false;
+
+    private LinkedList<Cell> adyacents;
 
     private void Start()
     {
@@ -15,15 +18,70 @@ public class PlayerMovement : CombatStats
     // Update is called once per frame
     void Update () {
 
-        if (Input.GetMouseButtonDown(0))
+        if (selected)
         {
-            RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~13)) //layer 13 click detection
+            if (Input.GetMouseButtonDown(0))
             {
-                nav.SetDestinationPlayerAndCost(hit.point); //update movements and move
+                RaycastHit hit;
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~13)) //layer 13 click detection
+                {
+                    Cell cellAux = GridMap.instance.CellFromWorldPoint(hit.point);
+
+                    if (adyacents.Contains(cellAux)) {
+                        nav.SetDestinationPlayerAndCost(cellAux.GlobalPosition);
+                    }
+                    //nav.SetDestinationPlayerAndCost(hit.point); //update movements and move
+                }
             }
         }
 	}
+
+    public void Select()
+    {
+        selected = true;
+        ShowAccesibles();
+    }
+
+    public void UnSelect()
+    {
+        selected = false;
+        adyacents.Clear();
+    }
+
+    public void ShowAccesibles()
+    {
+
+        byte cont = 0;
+        byte pasos;  //pasos que puede realizar
+        Cell cellActual = GridMap.instance.CellFromWorldPoint(transform.position);
+        float size = GridMap.instance.GetCellRadius() * 2;
+        findAccesibles(cellActual, cellActual, cont, pasos, size);
+
+    }
+
+    private void findAccesibles(Cell cell, Cell cellActual, int cont, byte pasos, float size) 
+    {
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                if (i != 0 && j != 0) { 
+                    Cell cellAux = GridMap.instance.CellFromWorldPoint(new Vector3(cell.GlobalPosition.x + i * size, cell.GlobalPosition.y, cell.GlobalPosition.z + j * size));
+
+                    if (cellAux != cellActual && (cont + cellAux.GetMovementCost()) < pasos)
+                    {
+                        if (!adyacents.Contains(cellAux))
+                        {
+                            adyacents.AddLast(cellAux);
+                        }
+
+                        findAccesibles(cellAux, cellActual, (cont + cellAux.GetMovementCost()), pasos, size);
+                    }
+                }
+            }
+        }
+
+    }
 }
---
+++ Assets/Scripts/PlayerTurn.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTurn : MonoBehaviour {
+
+    [SerializeField] private byte playerTurn;
+    //private byte lastTurn;
+    private CombatStats selected;
+
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = GetComponent<GameManager>();
+    }
+
+    void Update () {
+
+        playerTurn = gameManager.GetTurn();
+
+        if ( selected && playerTurn != selected.GetTeam())
+        {
+            selected.GetComponent<PlayerMovement>().UnSelect(); //No recuerdo si se podia hacer así
+            selected = null;
+        }
+        if (!selected)
+        {
+            SelectUnit();
+
+        } else {
+
+            if (Unselect())
+            {
+                selected.GetComponent<PlayerMovement>().UnSelect(); //No recuerdo si se podia hacer así
+                selected = null;
+
+            } else {
+
+                if (selected.GetUnityType() != CombatStats.UnitType.Torre && selected.GetUnityType() != CombatStats.UnitType.Castillo)      // es del tipo unidad
+                {
+                    if(selected.GetComponent<Units>().MovimientoDisponible())
+                    {
+                        //selected.GetComponent<PlayerMovement>().Select();   //No recuerdo si se podia hacer así
+                    } else
+                    {
+                        selected.GetComponent<PlayerMovement>().UnSelect(); //No recuerdo si se podia hacer así
+                    }
+                }
+
+                DoThings();
+
+                }
+            }
+        }
+	
+
+    private void SelectUnit()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~13))
+            {
+                Cell cell = GridMap.instance.CellFromWorldPoint(hit.point);
+                if (cell.unityOrConstructionOnCell && cell.unityOrConstructionOnCell.GetTeam() == playerTurn)
+                {
+                    selected = cell.unityOrConstructionOnCell;
+                    //selected.GetComponent<PlayerMovement>().Select();   //No recuerdo si se podia hacer así
+                }
+            }
+        }
+    }
+
+
+    private bool Unselect() {
+        if (Input.GetMouseButtonDown(1))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void DoThings() { }
+}
