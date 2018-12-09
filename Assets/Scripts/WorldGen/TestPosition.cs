a08e594c564309c8c646edd659af0eab77171461
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-12-09 18:15:43 +0100

    bug solved

---
+++ Assets/Scripts/WorldGen/TestPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPosition : MonoBehaviour {
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("2"))
+            Debug.Log(GridMap.instance.CellCordFromWorldPoint(transform.position) + " " +
+                GridMap.instance.grid[GridMap.instance.CellCordFromWorldPoint(transform.position).x, GridMap.instance.CellCordFromWorldPoint(transform.position).y].CellType);
+    }
+}
