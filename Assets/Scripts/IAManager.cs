e208de63877cd640c056ed6d0d24f2867d5f2365
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-12-13 11:39:13 +0100

    asd

---
+++ Assets/Scripts/IAManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StrategyList { GOLD_DOMAIN, GOLD_DEFENSE, CASTLE_DEFENSE, CASTLE_ATTACK, SKIRMISH, MAP_DOMAIN, REINFORCEMENT, STRENGTHEN }
+public struct UnitGroup
+{
+    GameObject[] UnitList;
+    StrategyList ActiveStrategy;
+}
+
+
+public class IAManager : MonoBehaviour {
+
+    public List<float[]> estrategyMapsList;
+    public UnitGroup[] unitGroups;
+}
