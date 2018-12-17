df769ed7b8c9cb22840c6945727885016bb64852
Fran Escriche Grau <franchupinazo@hotmail.es>
2018-12-17 11:27:53 +0100

    changes

--- Assets/Scripts/IAManager.cs
+++ Assets/Scripts/IAManager.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum StrategyList { GOLD_DOMAIN, GOLD_DEFENSE, CASTLE_DEFENSE, CASTLE_ATTACK, SKIRMISH, MAP_DOMAIN, REINFORCEMENT, STRENGTHEN }
-public enum DebugView{ nothing, basic_resources, terrain_cost, defense_pawn, defense_knight, defense_alfil, defense_general,
-    attack_pawn, attack_knight, attack_alfil, attack_general, better_position_general, enemy_tower_defense, ally_tower_defense,
+public enum DebugView{ nothing, basic_resources, terrain_cost, defense_pawn, defense_knight, defense_bishop, defense_general,
+    attack_pawn, attack_knight, attack_bishop, attack_general, better_position_general, enemy_tower_defense, ally_tower_defense,
     attack_tower, ally_heal_zone, enemy_heal_zone, ally_exploted_resources, enemy_exploted_resources, relative_force }
 public struct UnitGroup
 {
@@ -21,11 +21,11 @@ public class IAManager : MonoBehaviour {
       1 terrain cost
       2 defense pawn
       3 defense knight
-      4 defense alfil
+      4 defense bishop
       5 defense general
       6 attack pawn
       7 attack knight
-      8 attack alfil
+      8 attack bishop
       9 attack general
       10 better position general
       11 enemy tower defense
@@ -38,6 +38,7 @@ public class IAManager : MonoBehaviour {
       18 relative force
     */
     public List<UnitGroup> unitGroups;
+    public byte IAPlayer = 0;
     [SerializeField] private float decayMapDistancePerc = 0.1f;
     [SerializeField] private int decayMapDistanceInCells;
     [SerializeField] private float decayMapDistance;
@@ -56,6 +57,7 @@ public class IAManager : MonoBehaviour {
         decayMapDistanceInCells = Mathf.FloorToInt(decayMapDistance / (GridMap.instance.GetCellRadius() * 2f)) +1;
         //Set static maps
         SetResourcesMap();
+        SetBetterPositionGeneralMap();
     }
 
     private void SetResourcesMap()
@@ -78,6 +80,28 @@ public class IAManager : MonoBehaviour {
         }
     }
 
+    private void SetBetterPositionGeneralMap()
+    {
+        List<LinkedList<GameObject>> units = GameManager.instance.GetUnitList();
+        LinkedListNode<GameObject> unitsList = units[IAPlayer].First;
+        int i, j;
+        float factor;
+        for (; unitsList != null; unitsList = unitsList.Next)
+        {
+            i = GridMap.instance.CellCordFromWorldPoint(unitsList.Value.transform.position).x;
+            j = GridMap.instance.CellCordFromWorldPoint(unitsList.Value.transform.position).y;
+            for (int x = i - decayMapDistanceInCells >= 0 ? i - decayMapDistanceInCells : 0; x < GridMap.instance.GetGridSizeX() && x < i + decayMapDistanceInCells; x++)
+            {
+                for (int y = j - decayMapDistanceInCells >= 0 ? j - decayMapDistanceInCells : 0; y < GridMap.instance.GetGridSizeY() && y < j + decayMapDistanceInCells; y++)
+                {
+                    factor = Vector3.Distance(GridMap.instance.grid[i, j].GlobalPosition, GridMap.instance.grid[x, y].GlobalPosition) <= GridMap.instance.GetCellRadius() * 2 * CombatManager.instance.GetGeneralRange()?
+                        1 : 1 / Mathf.Pow(1 + Vector3.Distance(GridMap.instance.grid[i, j].GlobalPosition, GridMap.instance.grid[x, y].GlobalPosition) - GridMap.instance.GetCellRadius() * 2 * CombatManager.instance.GetGeneralRange(), 2);
+                    estrategyMapsList[10].SetValue((float)estrategyMapsList[10].GetValue(x, y) + factor, x, y);
+                }
+            }
+        }
+    }
+
     public void UpdateMaps()
     {
         
@@ -85,45 +109,37 @@ public class IAManager : MonoBehaviour {
 
     private void OnDrawGizmos()
     {
-        switch ((int)debugView)
+        if((int)debugView != 0)
         {
-            case 0:
+            float maxCost = float.MinValue;
+            float minCost = float.MaxValue;
+            foreach (float n in estrategyMapsList[(int)debugView + 1])
+            {
+                if (maxCost < n)
                 {
-                    break;
+                    maxCost = n;
                 }
-            case 1:
+                else if (minCost > n)
                 {
-                    float maxCost = float.MinValue;
-                    float minCost = float.MaxValue;
-                    foreach (float n in estrategyMapsList[0])
+                    minCost = n;
+                }
+            }
+            for (int i = 0; i < GridMap.instance.GetGridSizeX(); i++)
+            {
+                for (int j = 0; j < GridMap.instance.GetGridSizeY(); j++)
+                {
+                    if ((float)estrategyMapsList[(int)debugView + 1].GetValue(i, j) == float.MaxValue)
                     {
-                        if (maxCost < n)
-                        {
-                            maxCost = n;
-                        }
-                        else if (minCost > n)
-                        {
-                            minCost = n;
-                        }
+                        Gizmos.color = Color.black;
                     }
-                    for (int i = 0; i < GridMap.instance.GetGridSizeX(); i++)
+                    else
                     {
-                        for (int j = 0; j < GridMap.instance.GetGridSizeY(); j++)
-                        {
-                            if ((float)estrategyMapsList[0].GetValue(i,j) == float.MaxValue)
-                            {
-                                Gizmos.color = Color.black;
-                            }
-                            else
-                            {
-                                Gizmos.color = new Color((float)estrategyMapsList[0].GetValue(i, j) / maxCost, (float)estrategyMapsList[0].GetValue(i, j) / maxCost, (float)estrategyMapsList[0].GetValue(i, j) / maxCost, 1); ;
-                            }
+                        Gizmos.color = new Color((float)estrategyMapsList[(int)debugView + 1].GetValue(i, j) / maxCost, (float)estrategyMapsList[(int)debugView + 1].GetValue(i, j) / maxCost, (float)estrategyMapsList[(int)debugView + 1].GetValue(i, j) / maxCost, 1); ;
+                    }
 
-                            Gizmos.DrawCube(GridMap.instance.grid[i, j].GlobalPosition, Vector3.one * (GridMap.instance.GetCellRadius() * 2 * 19 / 20));
-                        }
-                    }
-                    break;
+                    Gizmos.DrawCube(GridMap.instance.grid[i, j].GlobalPosition, Vector3.one * (GridMap.instance.GetCellRadius() * 2 * 19 / 20));
                 }
+            }
         }
     }
 }
